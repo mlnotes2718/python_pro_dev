dce61318907925827a7004b251ad2f86c8bf6dbd
Thomas Tay <ai.thomas.tay@outlook.com>
2026-03-02 14:42:36 +0800

    update uv setup

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import subprocess
+
 def main():
     print("Hello from python-pro-dev!")
+
+    # run python script at src folder
+    subprocess.run(["python", "src/ruff_test.py"])
 
 
 if __name__ == "__main__":
